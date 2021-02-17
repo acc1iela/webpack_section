f5f8676d86834a9c2165ed289bea9328bae1760e
acc1iela <acc1iela@gmail.com>
2021-02-18 01:27:28 +0900

    browserlistrcの追加 polyfillの追加

--- soarflat/src/js/app.js
+++ soarflat/src/js/app.js
@@ -14,3 +14,17 @@ $('body')
 velocity($('h1'), 'fadeIn', { duration: 2000, loop: true });
 
 console.log(a);
+
+const z = { z: 3 };
+console.log({ x: 1, y: 2, ...z });
+
+const promise = new Promise((resolve) => {
+  setTimeout(() => resolve('hello!'), 3000);
+});
+
+async function delayHello() {
+  const value = await promise;
+  console.log(value);
+}
+
+delayHello();
